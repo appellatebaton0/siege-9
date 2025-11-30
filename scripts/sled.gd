84a1bfc0ce3d62666210c6f0e31bedd5c3988c0f
appellatebaton0@gmail.com <corneilusmaxfield@gmail.com>
2025-11-30 12:56:45 -0600

    v0.13 - Sled Animation
    Animated the sled so it crashes upon impact with anything

--- scripts/sled.gd
+++ scripts/sled.gd
@@ -14,10 +14,16 @@ func _on_body_entered(body: Node2D) -> void:
 	# KYS.
 	if body == player:
 		player.damage()
-		player.knockback = global_position.direction_to(player.global_position) * 35
+		player.knockback = global_position.direction_to(player.global_position) * 50
 	
-	speed = 0
+		speed /= 2
+	else: speed = 0
 	
-	queue_free() 
+	$Sprite2D.play("crash")
+	$Area2D.process_mode = Node.PROCESS_MODE_DISABLED
 	# Should happen aft. the animation finishes later
 	#, though collision should turn off for that.
+
+
+func _on_anim_finish() -> void:
+	queue_free()
