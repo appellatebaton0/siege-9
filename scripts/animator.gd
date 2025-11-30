98ccb77a35074e08e1d554c5a43433b374dac9bf
appellatebaton0@gmail.com <corneilusmaxfield@gmail.com>
2025-11-30 14:50:44 -0600

    v0.18 - Music & Fade
    Added music, and made it get quieter on the lose screen.

--- scripts/animator.gd
+++ scripts/animator.gd
@@ -1,7 +1,12 @@
 extends Node
 
+signal play(at:float)
+
 @export var anim_name := ""
+@export var a := 0.0
 
 @onready var animator:AnimationPlayer = get_tree().get_first_node_in_group("Animator")
 
 func animate() -> void: animator.play(anim_name)
+
+func _play(): play.emit(a)
